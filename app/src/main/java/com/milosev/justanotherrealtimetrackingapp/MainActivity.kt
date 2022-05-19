a27ce3973f759e59dbfe684280ad22b1e18de68e
Stanko Milošev <stanko@milosev.com>
2022-05-19 20:08:37 +0200

    added button listeners

--- app/src/main/java/com/milosev/justanotherrealtimetrackingapp/MainActivity.kt
+++ app/src/main/java/com/milosev/justanotherrealtimetrackingapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.milosev.justanotherrealtimetrackingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById<View>(R.id.btnStart) as Button
+        btnStart.setOnClickListener {
+
+        }
+
+        val btnStop: Button = findViewById<View>(R.id.btnStop) as Button
+        btnStop.setOnClickListener {
+        }
     }
 }
